6109f6dd71a58cf14894ab904edee8f259812b71
Aleksander Samól <aleksander.samol@gmail.com>
2020-04-02 21:33:47 +0200

    Use button tag in hamburger menu

--- src/Layout/HamburgerMenu/HamburgerMenu.tsx
+++ src/Layout/HamburgerMenu/HamburgerMenu.tsx
@@ -8,7 +8,7 @@ type HamburgerMenuProps = {
 const HamburgerMenu: React.FC<HamburgerMenuProps> = (props: HamburgerMenuProps) => {
 	return (
 		<div className={classes.HamburgerMenu}>
-			<a href="#" onClick={props.open}></a>
+			<button onClick={props.open}></button>
 		</div>
 	);
 };
