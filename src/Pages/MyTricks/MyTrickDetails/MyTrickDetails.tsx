010833b504827eb9ea4d91b41b27769acc3a3cc0
Aleksander Samól <aleksander.samol@gmail.com>
2020-04-13 16:56:56 +0200

    Added Video to details

--- src/Pages/MyTricks/MyTrickDetails/MyTrickDetails.tsx
+++ src/Pages/MyTricks/MyTrickDetails/MyTrickDetails.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import classes from "./MyTrickDetails.module.scss";
 import { MyTrick } from "../../Tricks/Trick/TrickTypes";
+import Video from "../../Tricks/Trick/Video/Video";
+import Trick from "../../Tricks/Trick/Trick";
 
 type MyTrickDetailsProps = {
 	trick?: MyTrick;
 };
 
 const MyTrickDetails: React.FC<MyTrickDetailsProps> = (props: MyTrickDetailsProps) => {
-	return (
-		<div className={classes.MyTrickDetails}>
-			<h2>{props.trick?.name}</h2>
-		</div>
-	);
+	const details = props.trick ? (
+		<>
+			<h2>{props.trick.name}</h2>
+			<div className={classes.Video}>
+				<Video url={props.trick.videoUrl} title={props.trick.name} />
+			</div>
+		</>
+	) : null;
+
+	return <div className={classes.MyTrickDetails}>{details}</div>;
 };
 
 export default MyTrickDetails;
