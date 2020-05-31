d1f156e6f6a9bce77835f740a77cbf827d62cd1f
Aleksander Samól <aleksander.samol@gmail.com>
2020-05-31 12:06:34 +0200

    Wrapped App in PreLoader

--- src/index.tsx
+++ src/index.tsx
@@ -13,13 +13,16 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
+import PreLoader from "./UI/PreLoader/PreLoader";
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PreLoader>
+				<App />
+			</PreLoader>
 		</Provider>
 	</React.StrictMode>,
 	document.getElementById("root")
